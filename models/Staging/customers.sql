db1c6f021688dadf67723e1c91d5a9856a5af16d
Radha Patidar <radhapatidar034@gmail.com>
2025-06-30 03:56:14 +0000

    c

--- models/Staging/customers.sql
+++ models/Staging/customers.sql
@@ -1,8 +1,10 @@
 {{config(materialized='table')}}
 with tb1 as (
-select id,
-first_name,
-last_name
+select customer_id,
+customer_name,
+customer_segment,
+email,
+ssn
 from {{source('datafeed_shared_schema','ROW_CUSTOMERS')}}
 )
 select * from tb1
